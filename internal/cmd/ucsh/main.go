e48865e46e2b995c544ff76d499feff13c37ed0e
Jeremías <jrmsdev@gmail.com>
2020-05-31 22:25:14 -0300

    ucsh main cmd

---
+++ internal/cmd/ucsh/main.go
@@ -0,0 +1,8 @@
+// Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
+// See LICENSE file.
+
+package ucsh
+
+func Main() {
+	println("ucsh main")
+}
---
+++ ucsh.go
@@ -0,0 +1,12 @@
+// Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
+// See LICENSE file.
+
+package main
+
+import (
+	"github.com/jrmsdev/ucsh/internal/cmd/ucsh"
+)
+
+func main() {
+	ucsh.Main()
+}
