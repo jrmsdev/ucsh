e3b6efe0b9bc579e957f263ab3a98a04c77bfbdd
Jeremías <jrmsdev@gmail.com>
2020-06-01 04:21:16 -0300

    parse and load config files

--- internal/cfg/cfg.go
+++ internal/cfg/cfg.go
@@ -4,6 +4,11 @@
 package cfg
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
 	"github.com/jrmsdev/ucsh/internal/log"
 )
 
@@ -31,12 +36,50 @@ var c = &ucsh{D: Config}
 
 var cinit = false
 
+var cfgfiles = []string{
+	"/etc/ucsh.cfg",
+	"/usr/local/etc/ucsh.cfg",
+	userCfgFile(),
+}
+
+func userCfgFile() string {
+	d, err := os.UserConfigDir()
+	if err != nil {
+		log.Panic(err)
+	}
+	return filepath.Join(d, "ucsh.cfg")
+}
+
+func load() error {
+	for _, fn := range cfgfiles {
+		blob, err := ioutil.ReadFile(fn)
+		if err != nil {
+			if os.IsNotExist(err) {
+				log.Debug(err)
+			} else {
+				log.Error(err)
+			}
+		} else {
+			err := json.Unmarshal(blob, c)
+			if err != nil {
+				log.Error(err)
+				return err
+			}
+			log.Debugf("loaded %s", fn)
+		}
+	}
+	return nil
+}
+
 func Init() error {
 	log.Debug("init")
 	if cinit {
 		log.Panic("config init already done")
 	}
 	cinit = true
+	if err := load(); err != nil {
+		return err
+	}
 	return nil
 }
 
