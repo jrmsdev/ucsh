38699e68f9711d1f935f0570c21124eb93971fa8
Jeremías <jrmsdev@gmail.com>
2020-06-11 18:32:10 -0300

    cmd main test config

--- cmd/ucsh/main_test.go
+++ cmd/ucsh/main_test.go
@@ -4,10 +4,17 @@
 package main
 
 import (
+	"path/filepath"
 	"testing"
 
 	//~ "github.com/jrmsdev/ucsh/internal/_ucsht"
 )
+
+func init() {
+	cfgfiles = []string{
+		filepath.FromSlash("testdata/ucsh.cfg"),
+	}
+}
 
 func TestMain(t *testing.T) {
 	main()
