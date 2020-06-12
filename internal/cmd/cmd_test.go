df870bd812014f36fc8248ccc767bc6567cc91cd
Jeremías <jrmsdev@gmail.com>
2020-06-12 01:11:53 -0300

    add test files

---
+++ internal/cmd/cmd_test.go
@@ -0,0 +1,11 @@
+// Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
+// See LICENSE file.
+
+package cmd
+
+import (
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+}
---
+++ internal/log/log_test.go
@@ -0,0 +1,11 @@
+// Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
+// See LICENSE file.
+
+package log
+
+import (
+	"testing"
+)
+
+func TestLog(t *testing.T) {
+}
