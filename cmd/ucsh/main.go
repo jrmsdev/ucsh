773c42e8c17dd17662a5f32b2fcbcb6804278d39
Jeremías <jrmsdev@gmail.com>
2020-06-16 18:25:07 -0300

    parse cmd flags

--- cmd/ucsh/main.go
+++ cmd/ucsh/main.go
@@ -5,15 +5,21 @@ package main
 
 import (
 	"github.com/jrmsdev/ucsh/internal/cmd"
+	"github.com/jrmsdev/ucsh/internal/cmd/flags"
 	"github.com/jrmsdev/ucsh/internal/log"
 	"github.com/jrmsdev/ucsh/internal/ucsh"
 )
 
 func main() {
 	log.Debug("start")
+
+	parser := flags.New("ucsh")
+	flags.Parse(parser)
+
 	sh := ucsh.New()
 	cmd.Configure(sh)
 	cmd.UserLoad(sh)
 	cmd.UserConfig(sh)
+
 	log.Debug("end")
 }
