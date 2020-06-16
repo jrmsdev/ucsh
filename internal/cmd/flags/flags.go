8ea5881065582b0439e41362686ff2fb4972a993
Jeremías <jrmsdev@gmail.com>
2020-06-16 18:22:03 -0300

    cmd show version information

--- internal/cmd/flags/flags.go
+++ internal/cmd/flags/flags.go
@@ -8,16 +8,19 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/jrmsdev/ucsh"
 	"github.com/jrmsdev/ucsh/internal/log"
 )
 
 var help bool
 var debug bool
+var version bool
 
 func New(name string) *flag.FlagSet {
 	f := flag.NewFlagSet(name, flag.ExitOnError)
 	f.BoolVar(&help, "h", false, "show this usage information and exit")
 	f.BoolVar(&debug, "d", false, "enable debug log")
+	f.BoolVar(&version, "V", false, "show version information and exit")
 	return f
 }
 
@@ -32,6 +35,9 @@ func Parse(f *flag.FlagSet) {
 	if help {
 		fmt.Fprintf(f.Output(), "Usage for %s:\n", f.Name())
 		f.PrintDefaults()
+		os.Exit(2)
+	} else if version {
+		fmt.Fprintf(f.Output(), "%s version %s\n", f.Name(), ucsh.Version())
 		os.Exit(2)
 	} else if debug {
 		log.SetDebug(true)
--- ucsh.go
+++ ucsh.go
@@ -2,3 +2,19 @@
 // See LICENSE file.
 
 package ucsh
+
+import (
+	"fmt"
+)
+
+const VMajor = 0
+const VMinor = 0
+const VPatch = 0
+
+func Version() string {
+	v := fmt.Sprintf("%d.%d", VMajor, VMinor)
+	if VPatch > 0 {
+		v = fmt.Sprintf("%s.%d", v, VPatch)
+	}
+	return v
+}
