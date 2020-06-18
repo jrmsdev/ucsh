4c6bde5a728937c9927e655d4213d6777948a052
Jeremías <jrmsdev@gmail.com>
2020-06-17 22:45:50 -0300

    config list split code

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,7 +7,6 @@ import (
 	"encoding/json"
 	"io"
 	"io/ioutil"
-	"strings"
 
 	"github.com/jrmsdev/ucsh/internal/log"
 )
@@ -41,37 +40,4 @@ func (c *Config) Load(name string, fh io.Reader) error {
 		return err
 	}
 	return nil
-}
-
-func (c *Config) List(prefix string) map[string]string {
-	log.Debugf("list '%s'", prefix)
-	l := make(map[string]string)
-	items := strings.SplitAfterN(prefix, ".", 2)
-	prefix = items[0]
-	filter := ""
-	if len(items) == 2 {
-		filter = items[1]
-	}
-	log.Debugf("list prefix='%s' filter='%s'", prefix, filter)
-	if prefix == "" || strings.HasPrefix(prefix, "container") {
-		for k, v := range c.ls(c.Container.list(), filter) {
-			l["container."+k] = v
-		}
-	}
-	if prefix == "" || strings.HasPrefix(prefix, "user") {
-		for k, v := range c.ls(c.User.list(), filter) {
-			l["user."+k] = v
-		}
-	}
-	return l
-}
-
-func (c *Config) ls(src map[string]string, filter string) map[string]string {
-	l := make(map[string]string)
-	for k, v := range src {
-		if filter == "" || strings.HasPrefix(k, filter) {
-			l[k] = v
-		}
-	}
-	return l
 }
---
+++ internal/config/list.go
@@ -0,0 +1,43 @@
+// Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
+// See LICENSE file.
+
+package config
+
+import (
+	"strings"
+
+	"github.com/jrmsdev/ucsh/internal/log"
+)
+
+func (c *Config) List(prefix string) map[string]string {
+	log.Debugf("list '%s'", prefix)
+	l := make(map[string]string)
+	items := strings.SplitAfterN(prefix, ".", 2)
+	prefix = items[0]
+	filter := ""
+	if len(items) == 2 {
+		filter = items[1]
+	}
+	log.Debugf("list prefix='%s' filter='%s'", prefix, filter)
+	if prefix == "" || strings.HasPrefix(prefix, "container") {
+		for k, v := range c.ls(c.Container.list(), filter) {
+			l["container."+k] = v
+		}
+	}
+	if prefix == "" || strings.HasPrefix(prefix, "user") {
+		for k, v := range c.ls(c.User.list(), filter) {
+			l["user."+k] = v
+		}
+	}
+	return l
+}
+
+func (c *Config) ls(src map[string]string, filter string) map[string]string {
+	l := make(map[string]string)
+	for k, v := range src {
+		if filter == "" || strings.HasPrefix(k, filter) {
+			l[k] = v
+		}
+	}
+	return l
+}
